5589a98998f0e5d393373c2146aa9cc61696708a
Nicat <snicat894@gmail.com>
2025-04-18 01:00:32 +0400

    Update function added to project

--- src/main/java/com/influencer/education/student/StudentController.java
+++ src/main/java/com/influencer/education/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.influencer.education.student;
 import com.influencer.education.student.entity.Student;
+import com.influencer.education.student.entity.University;
 import com.influencer.education.student.repo.StudentRepoData;
 import com.influencer.education.student.repo.CustomStudentRepo;
 import org.springframework.web.bind.annotation.*;
@@ -12,11 +13,11 @@ public class StudentController {
 
     private final CustomStudentRepo studentRepo;
 
-    private final StudentRepoData myDataStudentRepo;
+    private final StudentRepoData dataRepo;
 
-    public StudentController(final CustomStudentRepo studentRepo, final StudentRepoData myDataStudentRepo) {
+    public StudentController(final CustomStudentRepo studentRepo, final StudentRepoData dataRepo) {
         this.studentRepo = studentRepo;
-        this.myDataStudentRepo = myDataStudentRepo;
+        this.dataRepo = dataRepo;
     }
 
     @GetMapping
@@ -30,12 +31,31 @@ public class StudentController {
     }
 
     @PostMapping
-    public void save(@RequestBody Student student) {
-        myDataStudentRepo.save(student);
+    public void insert(@RequestBody Student student) {
+        dataRepo.save(student);
+    }
+    @PutMapping
+    public void update(@RequestBody Student student) {
+        dataRepo.save(student);
     }
 
     @DeleteMapping
     public void delete(@RequestParam Integer id) {
-        myDataStudentRepo.deleteById(id);
+        dataRepo.deleteById(id);
     }
+
+    @GetMapping("{id}")
+    public Student findById(@PathVariable("id") Integer id) {
+        return dataRepo.findById(id).isPresent() ?
+                dataRepo.findById(id).get() : null;
+    }
+
+    @GetMapping("{id}/universities/{universityId}")
+    public University findByIdUniversity(
+            @PathVariable("id") Integer id,
+            @PathVariable("universityId") Integer universityId) {
+        return dataRepo.findById(id).isPresent() ?
+                dataRepo.findById(id).get().getUniversity() : null;
+    }
+
 }
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,8 +1,11 @@
+
+var allStudents;
 function getStudents(){
     const xhttp = new XMLHttpRequest();
     function MyFunction(){
         const responseText = this.responseText;
         const responseArr = JSON.parse(responseText);
+        allStudents = responseArr;
         var tr = document.getElementById("tbl-students-body-tr").innerHTML;
         var tblBody = "";
         for(var i = 0; i < responseArr.length; i++){
@@ -10,6 +13,7 @@ function getStudents(){
 
             tblBody += tr
                 .replaceAll(":id", student.id)
+                .replaceAll(":index", i)
                 .replace(":firstName", student.firstName)
                 .replace(":surname", student.surname)
                 .replace(":email", student.email)
@@ -45,6 +49,8 @@ function getUniversities(){
         }
 
         document.getElementById("university_id").innerHTML = optionsHtml;
+        document.getElementById("create_university_id").innerHTML = optionsHtml;
+        document.getElementById("update_university_id").innerHTML = optionsHtml;
     }
     xhttp.onload = MyFunction;
     xhttp.open("GET","http://localhost:9090/education/university");
@@ -68,6 +74,60 @@ function deleteStudent(id){
     }
     xhttp.open("DELETE","http://localhost:9090/education/students?id="+id);
     xhttp.send();
+}
+function insertStudent(id){
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function (){
+        if( xhttp.status = "200"){
+            getStudents();
+        }
+    }
+
+    var student = {
+        firstName: document.getElementById("create_name").value,
+        surname: document.getElementById("create_surname").value,
+        email: document.getElementById("create_email").value,
+        age: document.getElementById("create_age").value,
+        university: {
+            id: document.getElementById("create_university_id").value
+        }
+    }
+    xhttp.open("POST","http://localhost:9090/education/students");
+    xhttp.setRequestHeader("Content-Type","application/json");
+    xhttp.send(JSON.stringify(student));
+}
+
+function updateStudent(id){
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function (){
+        if( xhttp.status = "200"){
+            getStudents();
+        }
+    }
+
+    var student = {
+        id: selectedId,
+        firstName: document.getElementById("update_name").value,
+        surname: document.getElementById("update_surname").value,
+        email: document.getElementById("update_email").value,
+        age: document.getElementById("update_age").value,
+        university: {
+            id: document.getElementById("update_university_id").value
+        }
+    }
+    xhttp.open("PUT","http://localhost:9090/education/students");
+    xhttp.setRequestHeader("Content-Type","application/json");
+    xhttp.send(JSON.stringify(student));
+}
+
+function selectForUpdate(index){
+    var selectedStudent = allStudents[index];
+    document.getElementById("update_name").value =  selectedStudent.firstName;
+    document.getElementById("update_surname").value =  selectedStudent.surname;
+    document.getElementById("update_email").value =  selectedStudent.email;
+    document.getElementById("update_age").value =  selectedStudent.age;
+    document.getElementById("update_university_id").value =  selectedStudent.university.id;
+    selectedId = selectedStudent.id;
 }
 
 
