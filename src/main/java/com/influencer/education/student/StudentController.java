9e00e351c248933a67b77332f4d7a493287704f6
Nicat <snicat894@gmail.com>
2025-04-19 17:54:21 +0400

    refactor: introduced StudentDTO with mapping methods and integrated across layers

--- src/main/java/com/influencer/education/student/StudentController.java
+++ src/main/java/com/influencer/education/student/StudentController.java
@@ -1,61 +1,88 @@
 package com.influencer.education.student;
+import com.influencer.education.student.dto.StudentDTO;
+import com.influencer.education.student.dto.UniversityDTO;
 import com.influencer.education.student.entity.Student;
 import com.influencer.education.student.entity.University;
 import com.influencer.education.student.repo.StudentRepoData;
 import com.influencer.education.student.repo.CustomStudentRepo;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/students")
 public class StudentController {
 
     private final CustomStudentRepo studentRepo;
-
     private final StudentRepoData dataRepo;
 
     public StudentController(final CustomStudentRepo studentRepo, final StudentRepoData dataRepo) {
-        this.studentRepo = studentRepo;
         this.dataRepo = dataRepo;
+        this.studentRepo = studentRepo;
+
     }
 
     @GetMapping
-    public List<Student> getList(
+    public List<StudentDTO> getList(
           @RequestParam(required = false) String name,
           @RequestParam(required = false) String surname,
           @RequestParam(required = false) String email ,
           @RequestParam(required = false, name = "university_id") Integer universityId,
           @RequestParam(required = false) Integer age){
-          return studentRepo.getList(name,surname,email,universityId,age);
+
+          return studentRepo.getList(name, surname, email, universityId, age)
+                  .stream()
+                  .map(StudentController ::toStudentDTO)
+                  .collect(Collectors.toList());
     }
 
     @PostMapping
-    public void insert(@RequestBody Student student) {
-        dataRepo.save(student);
+    public void insert(@RequestBody StudentDTO studentDto) {
+        dataRepo.save(toStudentEntity(studentDto));
     }
     @PutMapping
-    public void update(@RequestBody Student student) {
-        dataRepo.save(student);
+    public void update(@RequestBody StudentDTO studentDto) {
+        dataRepo.save(toStudentEntity(studentDto));
     }
 
     @DeleteMapping
+
     public void delete(@RequestParam Integer id) {
         dataRepo.deleteById(id);
     }
 
     @GetMapping("{id}")
-    public Student findById(@PathVariable("id") Integer id) {
-        return dataRepo.findById(id).isPresent() ?
-                dataRepo.findById(id).get() : null;
+    public StudentDTO findById(@PathVariable("id") Integer id) {
+        return dataRepo.findById(id).map(StudentController ::toStudentDTO).orElse(new StudentDTO());
     }
 
-    @GetMapping("{id}/universities/{universityId}")
-    public University findByIdUniversity(
-            @PathVariable("id") Integer id,
-            @PathVariable("universityId") Integer universityId) {
-        return dataRepo.findById(id).isPresent() ?
-                dataRepo.findById(id).get().getUniversity() : null;
+    private static StudentDTO toStudentDTO(Student student) {
+        return new StudentDTO()
+                .setId(student.getId())
+                .setFirstName(student.getFirstName())
+                .setSurname(student.getSurname())
+                .setAge(student.getAge())
+                .setEmail(student.getEmail())
+                .setUniversity(
+                        new UniversityDTO()
+                                .setId(student.getUniversity().getId())
+                                .setName(student.getUniversity().getName())
+                );
+    }
+    private static Student toStudentEntity(StudentDTO studentDto) {
+        return new Student()
+                .setId(studentDto.getId())
+                .setFirstName(studentDto.getFirstName())
+                .setSurname(studentDto.getSurname())
+                .setAge(studentDto.getAge())
+                .setEmail(studentDto.getEmail())
+                .setUniversity(
+                        new University()
+                                .setId(studentDto.getUniversity().getId())
+                                .setName(studentDto.getUniversity().getName())
+                );
     }
 
 }
---
+++ src/main/java/com/influencer/education/student/dto/StudentDTO.java
@@ -0,0 +1,77 @@
+package com.influencer.education.student.dto;
+
+import com.influencer.education.student.entity.University;
+
+public class StudentDTO {
+    private  Integer id;
+    private  String firstName;
+
+    private  String surname;
+    private  Integer age;
+    private  String email;
+    private  String password;
+    private UniversityDTO university;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public StudentDTO setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public StudentDTO setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public StudentDTO setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public StudentDTO setAge(Integer age) {
+        this.age = age;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public StudentDTO setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public StudentDTO setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public UniversityDTO getUniversity() {
+        return university;
+    }
+
+    public StudentDTO setUniversity(UniversityDTO university) {
+        this.university = university;
+        return this;
+    }
+}
